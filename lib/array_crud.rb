6c2bfcdbeb4a83df8c4a5aa2e83fed602bae473d
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-06 01:10:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,30 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["katie", "robb", "syd", "eric"]
-  array << "element"
+  array.push("casey")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("element")
+  array.unshift("robinson")
 end
 
 def remove_element_from_end_of_array(array)
-
+  casey = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  katie = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[4]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.last
 end
